b5da8ad3472ddd3b7255f9ca552d3d2bab3cd7f6
Jacob Seiler <seiler.jacob1@gmail.com>
2020-01-30 03:14:51 -0500

    Now works with multiple arguments

--- oneString.sh
+++ oneString.sh
@@ -1,12 +1,18 @@
-if [ $# -le 1 ]
+if [[ $# < 2 ]]
 then
     echo "Error"
     exit
 fi
 
-if [ $1 == $2 ]
-then
-    echo "They are the same"
-else
-    echo "They are different"
-fi
+CHECK=$1
+
+for arg in $*
+do
+    if [[ $arg != $CHECK ]]
+    then
+        echo "They are different"
+        exit
+    fi
+done
+
+echo "They are the same"
